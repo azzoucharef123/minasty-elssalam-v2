09becdeb0a81ee9b9f56ba75d6184e52a8619a5d
azzouchy12-bit <azzouchy12-bit@users.noreply.github.com>
2026-08-20 00:14:31 +0000

    Fix 502 error: import reconcileParentSofizPayPayment in paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -6,6 +6,7 @@ const {
   getTeacherElectronicPayments,
   dismissTeacherElectronicPayment,
   reconcileTeacherElectronicPayment,
+  reconcileParentSofizPayPayment,
   receiveSofizPayWebhook,
 } = require("../controllers/paymentController");
 
